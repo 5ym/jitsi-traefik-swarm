9f4c7be077cca309f855d013d4f4a291f0c24b1b
Ruk <16yuim@gmail.com>
2023-04-12 15:40:47 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir -p jitsi
+mkdir -p jitsi/web jitsi/transcripts jitsi/prosody/config jitsi/prosody/prosody-plugins-custom jitsi/jicofo jitsi/jvb
 curl https://raw.githubusercontent.com/jitsi/docker-jitsi-meet/master/env.example > jitsi/.env
 curl https://raw.githubusercontent.com/jitsi/docker-jitsi-meet/master/gen-passwords.sh > jitsi/gen-passwords.sh
 chmod +x jitsi/gen-passwords.sh
